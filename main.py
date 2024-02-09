a461d0277b35006e37359cd0c47af56b5770623b
JamesDorg <james.estabrook@students.fhu.edu>
2024-02-09 09:58:09 -0600

    Coded
    I works first try!!

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 #REMOVE PASS AND FIX THIS FUNCTION
-def anagram():
-    pass
+def anagram(str1, str2):
+
+    #NotSpace
+    if str1.isspace() or str2.isspace():
+        return False
+    
+    #DoctorStrings
+    str1 = str1.replace(' ', '')
+    str1 = str1.lower()
+    str2 = str2.replace(' ', '')
+    str2 = str2.lower()
+
+    #SplitStrings
+    lst1 = list(str1)
+    lst2 = list(str2)
+
+    #ChechCharacters
+    for i in lst1:
+        if i in lst2:
+            lst2.remove(i)
+        else:
+            return False
+
+    #Return
+    return True
+
 
 if __name__ == '__main__':
-    #REMOVE PASS YOUR CODE GOES HERE
-    pass
-    
+
+    #Two inputs
+
+    Imp1 = input("Enter first word: ")
+    Imp2 = input("Enter second word: ")
+
+    #Print function
+
+    print(anagram(Imp1, Imp2))
